38bf28538a350b96bb3867db8f585948819dc3a8
Danielle <deegreene2110@gmail.com>
2023-07-06 14:23:20 -0400

    this one was hard

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -2,35 +2,73 @@ package io.zipcoder.microlabs.mastering_loops;
 
 
 public class NumberUtilities {
+    public static boolean isOdd(int n) {
+        return (n % 2 != 0);
+    }
+
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+
+        String result = "";
+
+        for (int i = start; i < stop; i++) {
+
+            if (!isOdd(i)) {
+                result = result + i;
+            }
+        }
+        return result;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = start; i < stop; i++) {
+            if (isOdd(i)) {
+                sb.append(i);
+            }
+        }
+        return sb.toString();
     }
 
 
-    public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+
+        public static String getSquareNumbers ( int start, int stop, int step){
+            return getExponentiations(start, stop, step, 2);
+        }
+
+        public static String getRange ( int stop){
+
+            //Declare a string
+            String poppi = "";
+
+            int i;
+            //set for loop conditions
+
+            for (i = 0; i < stop; i++) {
+
+                poppi += i;
+            }
+
+            return poppi;
+        }
+
+        public static String getRange ( int start, int stop){
+            return getRange(start, stop, 1);
+        }
+
+
+        public static String getRange ( int start, int stop, int step){
+            return getExponentiations(start, stop, step, 1);
+        }
+
+
+        public static String getExponentiations ( int start, int stop, int step, int exponent){
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = start; i < stop; i += step) {
+                sb.append((int) Math.pow(i,exponent));
+            }
+            return sb.toString();
+        }
     }
-
-    public static String getRange(int start) {
-        return null;
-    }
-
-    public static String getRange(int start, int stop) {
-        return null;
-    }
-
-
-    public static String getRange(int start, int stop, int step) {
-        return null;
-    }
-
-
-    public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
-    }
-}
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,45 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
-    }
-}
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int row = 1; row <= tableSize; row++) {
+
+
+            for (int column = 1; column <= tableSize; column++) {
+
+                if (row * column < 10) {
+
+                    sb.append("  " + row * column + " ");
+
+                    sb.append("|");
+                }
+
+                if (row * column < 100 && row * column >= 10) {
+                    sb.append(" " + row * column + " ");
+
+                    sb.append("|");
+                }
+
+                if (row * column >= 100 ) {
+                    sb.append(row * column + " ");
+
+                    sb.append("|");
+
+                }
+            }
+            sb.append("\n");
+            }
+                return sb.toString();
+            }
+        }
